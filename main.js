a017fb74ca3b8dccc4ebc6856dd43c2177b82fc6
beeva-sergiosantamaria <sergio.santamaria@beeva.com>
2018-10-28 20:35:31 +0100

    emit event

--- main.js
+++ main.js
@@ -1,30 +1,23 @@
-var express = require('express');
+'use strict'
+var express = require('express'); 
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server, { origins: '*:*' });
 
 var socketPort = process.env.PORT || 3031;
 
-var messages = [{
-	author: "Carlos",
-    text: "Hola! que tal?"
-},{
-	author: "Pepe",
-    text: "Muy bien! y tu??"
-},{
-	author: "Paco",
-    text: "Genial!"
-}];
+var socketIO;
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server, { origins: '*:*', forceNew: true });
 
 server.listen(socketPort, function() {
-	console.log('Servidor corriendo en http://localhost:8080');
+	console.log('socket server running on 3031 port');
 });
 
 io.on('connection', function(socket) {
+    socketIO = socket;
 	console.log('Un cliente se ha conectado');
-    //socket.emit('messages', messages);
-    socket.on('UserDatas', function(data) {
+    socketIO.on('UserDatas', function(data) {
         console.log(data);
-        //socket.emit('messages', messages);
+        io.emit("messages", data);
     });
 });
---
+++ other.js
@@ -0,0 +1,72 @@
+'use strict'
+var express = require("express");  
+var bodyParser  = require("body-parser");
+var mongoose = require('mongoose');
+var methodOverride = require("method-override");
+var app = express();
+var socketio;
+
+var ServerPort = process.env.PORT || 3000;
+var socketPort = process.env.PORT || 3031;
+
+mongoose.connect('mongodb://localhost', function(err, res) {  
+  if(err) {
+    console.log('ERROR: connecting to Database. ' + err);
+  }
+  console.log('respuesta mongo: ', res);
+});
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "'GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.json());  
+app.use(methodOverride());
+
+var faceModel	= require('./models/faceCollectionMod')(app, mongoose);  
+var faceCtrl 	= require('./controllers/faceCollectionCtrl');
+
+var faces = express.Router();
+
+faces.route('/faceCollection')
+	.get(faceCtrl.findAllFaces)
+	.post(faceCtrl.addFaces)
+  .delete(faceCtrl.deleteFaces);
+
+app.use('/faces', faces);
+ 
+app.listen(ServerPort, function() {
+	console.log("Node server running");
+});
+
+var sockerServer = require('http').Server(app);
+var io = require('socket.io')(sockerServer, { origins: '*:*', forceNew: true });
+
+sockerServer.listen(socketPort, function() {
+  console.log("websocket server running");
+});
+
+io.on('connection', function(socket){
+  socketio = socket;
+  console.log('alguien se ha conectado con socket.');
+  socketio.on('zombis', function(data){
+    console.log('mensaje recibido de defenders: ',data);
+    io.emit("toSmartMirror", data);
+  });
+  socketio.on('armas', function(data){
+    console.log('mensaje recibido de explorers: ',data);
+    io.emit("toDefenders", data);
+  });
+  socketio.on('timeOut', function(data){
+    console.log('mensaje recibido de smartmirror por timeout: ',data);
+    io.emit("toExplorersTime", data);
+  });
+  socketio.on('requestWeapons', function(data){
+    console.log('mensaje recibido de defender pidiendo armas: ',data);
+    io.emit("toExplorersWeapon", data);
+  });
+});
